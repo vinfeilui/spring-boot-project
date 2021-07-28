3c68fa37576d4e7549a892da7b4ca23f3e2c5f5e
Vincent Lui <vinfeilui@gmail.com>
2021-07-29 00:36:27 +0800

    first

---
+++ src/main/java/com/vincent/lui/LuiApplication.java
@@ -0,0 +1,13 @@
+package com.vincent.lui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vincent/lui/LuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.lui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
