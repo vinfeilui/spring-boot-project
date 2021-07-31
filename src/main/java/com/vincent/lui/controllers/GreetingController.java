698f02c8d6ced5dc5eee175c2efc87d6833f2d8b
Vincent Lui <vinfeilui@gmail.com>
2021-07-31 16:50:26 +0800

    31/7

---
+++ src/main/java/com/vincent/lui/controllers/GreetingController.java
@@ -0,0 +1,20 @@
+package com.vincent.lui.controllers;
+
+
+import com.vincent.lui.models.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "fucking";
+    }
+}
